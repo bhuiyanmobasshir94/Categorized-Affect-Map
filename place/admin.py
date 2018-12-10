92637d3ff2c05b1e9535d032a15b0977efdb7520
Mobasshir Bhuiyan Shagor <mobasshirbhuiyan.shagor@gmail.com>
2018-12-11 00:29:31 +0600

    Second app update ... Admin panel

--- place/admin.py
+++ place/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Area
+from .models import Area, Location
 
-admin.site.register(Area)
+class LocationInline(admin.TabularInline):
+    model = Location
+    extra = 3
+
+class AreaAdmin(admin.ModelAdmin):
+    #     search_fields = ['question_text']
+    #     list_filter = ['pub_date']
+    #     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    #     fieldsets = [
+    #     (None,               {'fields': ['question_text']}),
+    #     ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    # ]
+        inlines = [LocationInline]
+
+admin.site.register(Area, AreaAdmin)
