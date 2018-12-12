350f72e223292fa669cea9a9367e1a9969d08b0b
Mobasshir Bhuiyan Shagor <mobasshirbhuiyan.shagor@gmail.com>
2018-12-13 04:50:33 +0600

    Map model and map codes integration

--- map/views.py
+++ map/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+
+from classification.models import Category, Feature
+from place.models import Area, Location
+from response.models import AffectiveResponse
+from forecast.models import Weather
+from .models import Master
+
+def index(request):
+    context = {
+    }
+    return render(request,'map/index.html',context)
