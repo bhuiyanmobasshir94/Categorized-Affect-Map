13720b6f348acc473151f984ca64a065184f29e4
Mobasshir Bhuiyan Shagor <mobasshirbhuiyan.shagor@gmail.com>
2018-12-13 03:31:54 +0600

    Classification apps add to heroku

--- categorized_affect_map/settings.py
+++ categorized_affect_map/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'classification.apps.ClassificationConfig',
+    'classification.apps.ClassificationConfig',
     # 'place.apps.PlaceConfig',
     # 'forecast.apps.ForecastConfig',
     # 'response.apps.ResponseConfig',
