1dfef037afd86b5c603bad37e718f948e9a93e34
Mobasshir Bhuiyan Shagor <mobasshirbhuiyan.shagor@gmail.com>
2018-12-13 16:09:00 +0600

    Xframe integration and two templates addition

--- map/urls.py
+++ map/urls.py
@@ -5,6 +5,9 @@ app_name = 'map'
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('map-view/',views.map_view,name='map-view'),
+    path('category/',views.category,name='category'),
+    path('about/',views.about,name='about'),
     # path('<int:question_id>/',views.detail, name='detail'),
     # path('<int:question_id>/results/', views.results, name='results'),
     # path('<int:question_id>/vote/', views.vote, name='vote'),
--- map/views.py
+++ map/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.template import Context, loader
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 
 # Create your views here.
 
@@ -16,3 +18,20 @@ def index(request):
     context = {
     }
     return render(request,'map/index.html',context)
+
+def map_view(request):
+    
+    context = {
+    }
+    return render(request,'map/map_view.html',context)
+
+@xframe_options_sameorigin
+def category(request):
+    template = loader.get_template("map/category.html")
+    return HttpResponse(template.render())
+
+def about(request):
+    # context = {
+    # }
+    # return render(request,'map/index.html',context)
+    return HttpResponse("Hello to about")
